d6091311d63567e540d23dd1f2892df9d368fb99
Tat-T <ring_87_87@mail.ru>
2025-02-16 12:17:52 +0300

    modified:   Site/pages/upload.php

--- Site/pages/upload.php
+++ Site/pages/upload.php
@@ -24,3 +24,27 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_FILES["image"])) {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Загрузка изображения</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-info-subtle">
+<div class="container mt-5">
+    <h2 class="text-info">Загрузка изображения</h2>
+    <form  method="POST" class="container col-6 rounded p-3 mt-3 position-absolute top-50 start-50 translate-middle" enctype="multipart/form-data">
+        <div class="mb-3">
+            <input type="file" name="image" class="form-control" required>
+        </div>
+        <div class="d-grid mt-3">
+            <button type="submit" class="btn btn-primary">Загрузить</button>
+            <a href="../index.php" class="btn btn-secondary mt-3">На главную</a>
+        </div>
+    </form>
+</div>
+</body>
+</html>
